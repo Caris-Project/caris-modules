6468eb93effce034a1571a6644c4b7fedb65c493
Nam Huynh <namhq.1989@gmail.com>
2021-06-15 16:44:54 +0700

    update

---
+++ utils/echo_context.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"github.com/Caris-Project/caris-modules/constant"
+	"github.com/labstack/echo/v4"
+)
+
+// GetUserIDFromEchoContext ...
+func GetUserIDFromEchoContext(c echo.Context) string {
+	userIDInterface := c.Get(constant.EchoContextKeyUserID)
+	if userIDInterface == nil {
+		return ""
+	}
+	return userIDInterface.(string)
+}
+
+// GetStaffIDFromEchoContext ...
+func GetStaffIDFromEchoContext(c echo.Context) string {
+	staffIDInterface := c.Get(constant.EchoContextKeyStaffID)
+	if staffIDInterface == nil {
+		return ""
+	}
+	return staffIDInterface.(string)
+}
