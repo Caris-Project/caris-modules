ec671a1852924b6efa66be186838b8dcdba409f0
Nam Huynh <namhq.1989@gmail.com>
2021-06-16 21:03:09 +0700

    add admin

--- model/user.go
+++ model/user.go
@@ -45,25 +45,3 @@ type UserFacebook struct {
 	Name  string `json:"name"`
 	Email string `json:"email"`
 }
-
-// UserStats ...
-type UserStats struct {
-	ID              string `gorm:"primaryKey"`
-	Post            int
-	CurrentCoin     int
-	Reward          int
-	Car             int
-	Follower        int
-	Following       int
-	ReferralTotal   int
-	ReferralSuccess int
-
-	// Ref
-	User   User `gorm:"foreignKey:UserID"`
-	UserID string
-}
-
-// TableName overrides the table name
-func (UserStats) TableName() string {
-	return "user_stats"
-}
