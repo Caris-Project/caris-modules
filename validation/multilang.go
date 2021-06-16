0bbc704ba2deca6ab23b030ac4f149636bd4f2e1
Nam Huynh <namhq.1989@gmail.com>
2021-06-16 14:28:26 +0700

    update

---
+++ validation/multilang.go
@@ -0,0 +1,45 @@
+package validation
+
+import (
+	"encoding/json"
+	"errors"
+
+	"github.com/Caris-Project/caris-modules/constant"
+	"github.com/Caris-Project/caris-modules/model"
+)
+
+// MultilangRequired ...
+func MultilangRequired(v interface{}) (err error) {
+	err = errors.New("invalid multiple languages data")
+
+	value, ok := v.(model.MultiLang)
+	if !ok {
+		return
+	}
+
+	// Get byte data
+	byteData, _ := json.Marshal(value)
+
+	// Convert to map
+	var mapData map[string]string
+	if e := json.Unmarshal(byteData, &mapData); e != nil {
+		return
+	}
+
+	// Check language
+	hasInvalidLang := false
+	for _, langInterface := range constant.LangList {
+		lang := langInterface.(string)
+		if mapData[lang] == "" {
+			hasInvalidLang = true
+			break
+		}
+	}
+
+	// Set error
+	if !hasInvalidLang {
+		err = nil
+	}
+
+	return
+}
