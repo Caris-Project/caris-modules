b5dc00c987dd39c95da67ef7e0307eb642f6d047
Nam Huynh <namhq.1989@gmail.com>
2021-06-21 21:16:54 +0700

    add queue data

--- rabbitmq/queue_name.go
+++
@@ -1,7 +0,0 @@
-package rabbitmq
-
-// Queue name
-const (
-	QueueCreateAndPushSingleNotification = "notification:create_and_push_single"
-	QueuePushNotificationByTokens        = "notification:push_by_tokens"
-)
