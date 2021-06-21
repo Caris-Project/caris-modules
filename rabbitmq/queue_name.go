7a6db7877ae5b273d6097d869249bafce7180b3f
Nam Huynh <namhq.1989@gmail.com>
2021-06-21 20:38:13 +0700

    add more rabbitmq func

---
+++ rabbitmq/queue_name.go
@@ -0,0 +1,7 @@
+package rabbitmq
+
+// Queue name
+const (
+	QueueCreateAndPushSingleNotification = "notification:create_and_push_single"
+	QueuePushNotificationByTokens        = "notification:push_by_tokens"
+)
--- rabbitmq/rabbitmq.go
+++ rabbitmq/rabbitmq.go
@@ -1,7 +1,6 @@
 package rabbitmq
 
 import (
-	"encoding/json"
 	"fmt"
 
 	"github.com/logrusorgru/aurora"
@@ -38,68 +37,11 @@ func GetConnection() *amqp.Connection {
 }
 
 // Log ...
-func Log(queueName string) {
+func log(queueName string) {
 	fmt.Println(aurora.BgBlue("*** New message from queue: " + queueName))
-}
-
-// PublishMessage ...
-func PublishMessage(queueName string, data interface{}) error {
-	byteData, _ := json.Marshal(data)
-
-	// Set publishing data
-	message := generateData(byteData)
-
-	// Send
-	return channel.Publish(
-		"",        // exchange
-		queueName, // routing key
-		false,     // mandatory
-		false,     // immediate
-		message,
-	)
-}
-
-// generateData ...
-func generateData(body []byte) amqp.Publishing {
-	return amqp.Publishing{
-		ContentType: "application/json",
-		Body:        body,
-	}
 }
 
 // PublishErrorLog ...
 func PublishErrorLog(queueName string, err error) {
 	fmt.Println("*** Error when publish message to", queueName, err.Error())
-}
-
-// DeclareQueue ...
-func DeclareQueue(queueName string) {
-	_, err := channel.QueueDeclare(
-		queueName, // name
-		true,      // durable
-		false,     // delete when unused
-		false,     // exclusive
-		false,     // no-wait
-		nil,       // arguments
-	)
-
-	if err != nil {
-		fmt.Println("... Declare failed subscriber queue "+queueName, err)
-	} else {
-		fmt.Println(aurora.Green("*** RabbitMQ - Declared queue: " + queueName))
-	}
-}
-
-// GetMessagesFromQueue ...
-func GetMessagesFromQueue(queueName string) <-chan amqp.Delivery {
-	messages, _ := channel.Consume(
-		queueName,
-		"",
-		true,
-		false,
-		false,
-		false,
-		nil,
-	)
-	return messages
 }
