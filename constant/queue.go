06d1d9ff8d623815773891973b1935e01c4215db
Nam Huynh <namhq.1989@gmail.com>
2021-06-21 21:21:06 +0700

    add queue data

---
+++ constant/queue.go
@@ -0,0 +1,11 @@
+package constant
+
+// Constant
+const (
+	PriorityCritical = "critical"
+	PriorityDefault  = "default"
+	PriorityLow      = "low"
+
+	TaskCreateAndPushSingleNotification = "notification:create_and_push_single"
+	TaskPushNotificationByTokens        = "notification:push_by_tokens"
+)
