b43ef5993075b5dd8e3c6a2aa4de35896c840a55
Sinh <luuvansinh555@gmail.com>
2021-06-23 21:18:08 +0700

    add referral model

---
+++ model/referral.go
@@ -0,0 +1,22 @@
+package model
+
+import "time"
+
+// Referral ...
+type Referral struct {
+	ID        string `gorm:"primaryKey"`
+	UserID    string
+	InviteeID string
+	Success   bool
+	CreatedAt time.Time
+	UpdatedAt time.Time
+
+	// Ref
+	User    User `gorm:"foreignKey:UserID"`
+	Invitee User `gorm:"foreignKey:InviteeID"`
+}
+
+// TableName overrides the table name
+func (Referral) TableName() string {
+	return "referral"
+}
