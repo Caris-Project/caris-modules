6987ccd3b6748dcce6652ac249e2d7f7723243d4
Nam Huynh <namhq.1989@gmail.com>
2021-06-24 11:50:24 +0700

    add translation notification

--- translation/translation.go
+++ translation/translation.go
@@ -1,24 +1,18 @@
 package translation
 
 import (
-	"fmt"
 	"path"
 	"runtime"
 
 	"github.com/BurntSushi/toml"
 	"github.com/nicksnyder/go-i18n/v2/i18n"
-	"github.com/thoas/go-funk"
 	"golang.org/x/text/language"
-
-	"github.com/Caris-Project/caris-modules/constant"
 )
 
 type languageData struct {
 	Lang      string
 	Localizer *i18n.Localizer
 }
-
-var listOther = make([]languageData, 0)
 
 // Init ...
 func Init() {
@@ -28,6 +22,8 @@ func Init() {
 	// Load files
 	pwd := getPwd()
 	loadOther(bundle, pwd)
+	loadNotificationTitle(bundle, pwd)
+	loadNotificationContent(bundle, pwd)
 }
 
 func getPwd() string {
@@ -36,43 +32,4 @@ func getPwd() string {
 		panic("No caller information")
 	}
 	return path.Dir(filename)
-}
-
-func loadOther(bundle *i18n.Bundle, pwd string) {
-	for _, langInterface := range constant.LangList {
-		lang := langInterface.(string)
-
-		// Add language
-		path := fmt.Sprintf("%s/other/active.%s.toml", pwd, lang)
-		_, err := bundle.LoadMessageFile(path)
-		if err != nil {
-			panic(err)
-		}
-
-		// Add localizer
-		localizer := i18n.NewLocalizer(bundle, lang)
-
-		// Add item
-		item := languageData{
-			Lang:      lang,
-			Localizer: localizer,
-		}
-		listOther = append(listOther, item)
-	}
-}
-
-// GetOtherText ...
-func GetOtherText(lang, messageID string) string {
-	itemInterface := funk.Find(listOther, func(i languageData) bool {
-		return i.Lang == lang
-	})
-	if itemInterface == nil {
-		return ""
-	}
-
-	item := itemInterface.(languageData)
-	msg, _ := item.Localizer.Localize(&i18n.LocalizeConfig{
-		MessageID: messageID,
-	})
-	return msg
 }
