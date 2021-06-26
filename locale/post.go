35c8cf16d766aa5d5ca31d608f764ea8241e8280
Minh Nguyen <minh.milano1511@gmail.com>
2021-06-26 16:46:03 +0700

    locale-post

--- locale/post.go
+++ locale/post.go
@@ -2,8 +2,9 @@ package locale
 
 // Constant ...
 const (
-	PostKeyRequiredContent = "post_require_content"
-	PostKeyRequiredTitle   = "post_require_title"
+	PostKeyRequiredContent      = "post_require_content"
+	PostKeyRequiredTitle        = "post_require_title"
+	PostKeyRequiredBriefContent = "post_require_brief_content"
 )
 
 // 500-599
@@ -17,5 +18,10 @@ var post = []Locale{
 		Key:     PostKeyRequiredTitle,
 		Message: "Tiêu đề bài viết không được trống!",
 		Code:    501,
+	},
+	{
+		Key:     PostKeyRequiredBriefContent,
+		Message: "Nội dung bài viết ngắn không được trống!",
+		Code:    502,
 	},
 }
