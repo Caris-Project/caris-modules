b54f923b5ce3b62a229274cc1070c466fcaa5f16
Minh Nguyen <minh.milano1511@gmail.com>
2021-06-26 22:41:12 +0700

    locale-post-update

--- locale/post.go
+++ locale/post.go
@@ -5,6 +5,9 @@ const (
 	PostKeyRequiredContent      = "post_require_content"
 	PostKeyRequiredTitle        = "post_require_title"
 	PostKeyRequiredBriefContent = "post_require_brief_content"
+	PostKeyStatusNotPending     = "post_status_not_pending"
+	PostKeyIsPublishTimer       = "post_is_publish_timer"
+	PostKeyChangePublishError  = "post_change_publish_err"
 )
 
 // 500-599
@@ -23,5 +26,20 @@ var post = []Locale{
 		Key:     PostKeyRequiredBriefContent,
 		Message: "Nội dung bài viết ngắn không được trống!",
 		Code:    502,
+	},
+	{
+		Key:     PostKeyStatusNotPending,
+		Message: "Yêu cầu bài viết trạng thái chờ duyệt!",
+		Code:    503,
+	},
+	{
+		Key:     PostKeyIsPublishTimer,
+		Message: "Yêu cầu bài viết loại hẹn giờ!",
+		Code:    504,
+	},
+	{
+		Key:     PostKeyChangePublishError,
+		Message: "Đã xảy ra lỗi! Vui lòng thử lại!",
+		Code:    505,
 	},
 }
