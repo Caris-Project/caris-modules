371713b00d677d8b0a9ceed73379d08322ba808e
Minh Nguyen <minh.milano1511@gmail.com>
2021-07-04 22:26:59 +0700

    util-init-assets-folder

---
+++ utils/init_folder.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"os"
+	"path"
+)
+
+// InitFolder ...
+func InitFolder() {
+	dir, _ := os.Getwd()
+	uploadFilePath := path.Join(dir + "/assets")
+	createFolder(uploadFilePath)
+}
+
+func createFolder(path string) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		// path/to/whatever does not exist
+		_ = os.MkdirAll(path, os.ModePerm)
+	}
+}
