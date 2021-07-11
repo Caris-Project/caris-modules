d2173b3527ccd9fb6ea72135b33049db01fe7e51
Sinh <luuvansinh555@gmail.com>
2021-07-11 20:56:45 +0700

    update config

---
+++ constant/user_coin_history.go
@@ -0,0 +1,10 @@
+package constant
+
+// Types
+const (
+	CoinHistoryTypeRegisterSuccess        = "register_successfully"
+	CoinHistoryTypeReferralSuccess        = "referral_successfully"
+	CoinHistoryTypeAddCarSuccess          = "add_car_successfully"
+	CoinHistoryTypeInputReferralCode      = "input_referral_code"
+	CoinHistoryTypeExchangeVoucherSuccess = "exchange_voucher_successfully"
+)
